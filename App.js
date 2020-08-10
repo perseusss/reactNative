0dae83519fa6500aaa639bce57b12d05298a58de
perseusss <ed>
2020-08-10 17:34:40 +0300

    using props in react

---
+++ App.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, TextInput, Button} from 'react-native';
+// import {Image} from 'react-native';
+import AppNews from './AppNews';
+
+
+ export class App extends Component{
+
+  render(){
+        const news = [
+      {
+          title  : "FORD MUSTANG ",
+          description :"Daha benzersiz, daha yüksek performansa sahip ve daha yenilikçi Yeni Mustang deneyimine hazır olun. Çünkü efsane şimdi bir başka."
+      },
+      {
+        title : "LAMBORGHINI URUS",
+        description:"Lamborghini Urus is the first Super Sport Utility Vehicle in the world to merge the soul of a super sports car with the functionality of an SUV. Powered by a 4.0-liter twin-turbo V8 engine producing 650 CV and 850 Nm of torque, Urus accelerates from 0 to 62 mph in 3.6 seconds and reaches a top speed of 190 mph. The design, performance, driving dynamics and driving emotion flow effortlessly into this visionary approach to Lamborghini DNA."
+      },
+      {
+        title : "PORSCHE",
+        description:"Bu alıntı, Porsche'yi Porsche yapan her şeyin kalbine ulaşır. Bir marka olarak, bir firma olarak ve bir otomobil üreticisi olarak. 65 yıldan uzun süredir, bize yol gösteren yıldızımız oldu. Çalışmalarımızın ve araçlarımızın karakterini belirleyen tüm değerleri kapsar. Bu nedenle, bunu en iyi açıklayabilecek kişinin, Porsche adını taşıyan ilk spor otomobilin yaratıcısından başkası olamayacağına şaşırmaya gerek yok: Ferdinand Anton Ernst – veya kısaca, Ferry Porsche."
+      }
+      
+      ];
+    return(
+      <View style = {styles.container}>
+
+     <AppNews 
+              title = {news[0].title}
+              description = {news[0].description} />
+     <AppNews 
+              title = {news[1].title}
+              description = {news[1].description} />
+     <AppNews 
+              title = {news[2].title}
+              description = {news[2].description} />
+      
+      </View>
+
+    )
+  }
+  }
+  export default App;
+  
+
+
+  const styles = StyleSheet.create({
+    
+    container:{
+      flex : 1,
+      flexDirection: 'column',
+      paddingTop: 90,
+      paddingLeft:30,
+    },
+    headerTxt:{
+      paddingLeft:130,
+    },
+    paragraph:{
+      paddingTop:20,
+      paddingLeft:30,
+    },
+    contentWrapper:{
+      padding:20,
+      flexDirection:'column',
+      justifyContent:'center'
+     }
+    
+  })
---
+++ AppNews.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View,TextInput, Button} from 'react-native';
+import { Header } from 'react-native/Libraries/NewAppScreen';
+// import App from './App';
+
+class AppNews extends Component{
+    render(){
+        return(
+          <View >
+          
+          <Text>{this.props.title}</Text>
+          <View style={styless.contentWrapper}></View>
+        <Text>{this.props.description}</Text>
+        <View style={styless.spaceBetween}></View>
+
+          </View>
+        )
+    }
+}
+export default AppNews;
+
+const styless = StyleSheet.create({
+    contentWrapper:{
+        padding:1,
+        flexDirection:'column',
+        justifyContent:'center',
+        shadowColor:'black',
+        shadowOffset:{width: 1 , height:2},
+        shadowOpacity:0.5,
+        backgroundColor: 'black'
+       },
+       spaceBetween:{
+        padding:20,
+        flexDirection:'column',
+        justifyContent:'center'
+       }
+     
+})
